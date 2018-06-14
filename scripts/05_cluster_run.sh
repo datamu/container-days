19e2353432f7d37e914bbaa8ecd594fe6b9520b5
Krishna Kalyan <krishna.kalyan@zoi.de>
2018-06-14 14:55:16 +0200

    jar fix

--- scripts/05_cluster_run.sh
+++ scripts/05_cluster_run.sh
@@ -68,7 +68,7 @@ bin/spark-submit \
  --master k8s://http://127.0.0.1:8080 \
  --deploy-mode cluster \
  --name $APP_NAME \
- --class com.zoi.xgb.DemoDT \
+ --class com.zoi.xgb.DemoRF \
  --conf spark.executor.instances=5 \
  --conf spark.kubernetes.authenticate.driver.serviceAccountName=spark \
  --conf spark.kubernetes.container.image=gcr.io/t-zoi-play18/spark-k8:$TAG \
